ededa61214653a681eda07b6a2b20fba511ac227
Zaid <zaidzuhairalani@gmail.com>
2024-10-11 23:01:02 +0300

    Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,294 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  runApp(MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.black,
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: SingleChildScrollView(
+                child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.lock_outline,
+                            size: 15,
+                            color: Colors.white,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Text(
+                            "za1.d",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 20),
+                          ),
+                          SizedBox(
+                            width: 3,
+                          ),
+                          RotatedBox(
+                            quarterTurns: 3,
+                            child: Icon(
+                              Icons.arrow_back_ios,
+                              size: 10,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.add_box_outlined,
+                            size: 25,
+                            color: Colors.white,
+                          ),
+                          Icon(
+                            Icons.format_align_justify_outlined,
+                            size: 25,
+                            color: Colors.white,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 7,
+                  ),
+                  const Padding(
+                      padding: EdgeInsets.only(right: 15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          CircleAvatar(
+                            backgroundImage:
+                                AssetImage("images/profileimage.jpg"),
+                            radius: 45,
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "25",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 17),
+                              ),
+                              Text(
+                                "posts",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 11),
+                              )
+                            ],
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "237",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 17),
+                              ),
+                              Text(
+                                "followers",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 11),
+                              )
+                            ],
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "274",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 17),
+                              ),
+                              Text(
+                                "following",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 11),
+                              )
+                            ],
+                          ),
+                        ],
+                      )),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  const Text("Zaid Zuhair Alani",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 13,
+                          fontWeight: FontWeight.w500)),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  const Text(
+                    "9-January",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 12),
+                  ),
+                  const Text(
+                    "75% Network Engineer",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 12),
+                  ),
+                  const Text(
+                    "Junior Backend Developer (django, .NET)",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 12),
+                  ),
+                  const Text(
+                    "Junior AI Developer",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontSize: 12),
+                  ),
+                  const SizedBox(
+                    height: 6,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        alignment: Alignment.center,
+                        height: 20,
+                        width: 184,
+                        decoration: const BoxDecoration(
+                          borderRadius:
+                              BorderRadius.all(Radius.elliptical(3, 3)),
+                          color: Color.fromARGB(179, 53, 51, 51),
+                        ),
+                        child: const Text(
+                          "Edit profile",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700),
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.center,
+                        height: 20,
+                        width: 184,
+                        decoration: const BoxDecoration(
+                          borderRadius:
+                              BorderRadius.all(Radius.elliptical(3, 3)),
+                          color: Color.fromARGB(179, 53, 51, 51),
+                        ),
+                        child: const Text(
+                          "Share profile",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 12,
+                              fontWeight: FontWeight.w700),
+                        ),
+                      )
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 12,
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CircleAvatar(
+                        backgroundImage: AssetImage("images/cake.webp"),
+                        radius: 25,
+                      ),
+                      CircleAvatar(
+                        backgroundImage: AssetImage("images/cake.webp"),
+                        radius: 25,
+                      ),
+                      CircleAvatar(
+                        backgroundImage: AssetImage("images/cake.webp"),
+                        radius: 25,
+                      ),
+                      CircleAvatar(
+                        backgroundImage: AssetImage("images/cake.webp"),
+                        radius: 25,
+                      ),
+                      CircleAvatar(
+                        backgroundImage: AssetImage("images/cake.webp"),
+                        radius: 25,
+                      ),
+                      CircleAvatar(
+                        backgroundImage: AssetImage("images/cake.webp"),
+                        radius: 25,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 13,
+                  ),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Expanded(
+                          child: Icon(
+                        Icons.grid_on_outlined,
+                        size: 25,
+                        color: Colors.white,
+                      )),
+                      Expanded(
+                          child: Icon(
+                        Icons.person_pin_outlined,
+                        size: 25,
+                        color: Colors.white,
+                      ))
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(child: Image.asset('images/cake.webp')),
+                          Expanded(child: Image.asset('images/profileimage.jpg')),
+                          Expanded(child: Image.asset('images/cake.webp')),
+                        ],
+                      ),
+                      // SizedBox(height: 10),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(child: Image.asset('images/profileimage.jpg')),
+                          Expanded(child: Image.asset('images/cake.webp')),
+                          Expanded(child: Image.asset('images/profileimage.jpg')),
+                        ],
+                      ),
+                      // SizedBox(height: 10),
+                      // Row(
+                      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      //   children: [
+                      //     Image.asset('images/cake.webp',
+                      //         width: 100, height: 100),
+                      //     Image.asset('images/profileimage.jpg',
+                      //         width: 100, height: 100),
+                      //     Image.asset('images/cake.webp',
+                      //         width: 100, height: 100),
+                      //   ],
+                      // ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+          )))));
 }
